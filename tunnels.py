6ef3bdd829e1bee177481fe642c4f37d508a5295
Henning Jacobs <henning@jacobs1.de>
2015-07-13 16:54:38 +0200

    use click arg parsing

--- tunnels.py
+++ tunnels.py
@@ -3,23 +3,30 @@
 import click
 import json
 import subprocess
-import sys
 
 from clickclick import Action
 
-stack_name = sys.argv[1]
-port = int(sys.argv[2])
-jump_host = sys.argv[3]
 
-out = subprocess.check_output(['senza', 'instances', '--output=json', stack_name])
-data = json.loads(out.decode('utf-8'))
+@click.command()
+@click.argument('stack_name')
+@click.argument('port', type=int)
+@click.argument('jump_host')
+def cli(stack_name, port, jump_host):
+    out = subprocess.check_output(['senza', 'instances', '--output=json', stack_name])
+    data = json.loads(out.decode('utf-8'))
 
-opts = []
-for row in data:
-    ip = row['private_ip']
-    with Action('Adding IP {}..'.format(ip)):
-        subprocess.call(['sudo', 'ip', 'a', 'a', 'dev', 'lo', ip])
-        opts += ['-L', '{}:{}:{}:{}'.format(ip, port, ip, port)]
+    opts = []
+    for row in data:
+        ip = row['private_ip']
+        with Action('Adding IP {}..'.format(ip)):
+            subprocess.call(['sudo', 'ip', 'a', 'a', 'dev', 'lo', ip])
+            opts += ['-L', '{}:{}:{}:{}'.format(ip, port, ip, port)]
 
-click.secho('Starting SSH tunnels..', bold=True)
-subprocess.call(['ssh'] + opts + [jump_host, 'while true; do echo -n .; sleep 60; done'])
+    if not opts:
+        raise click.UsageError('No instances for Senza stack "{}" found.'.format(stack_name))
+
+    click.secho('Starting SSH tunnels..', bold=True)
+    subprocess.call(['ssh'] + opts + [jump_host, 'while true; do echo -n .; sleep 60; done'])
+
+if __name__ == '__main__':
+    cli()
