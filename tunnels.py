2218571c5fc753378b1d8876f80401faac0ff243
Henning Jacobs <henning@jacobs1.de>
2015-07-20 11:55:30 +0200

    print endpoints for copy&paste

--- tunnels.py
+++ tunnels.py
@@ -17,6 +17,7 @@ def cli(stack_name, port, jump_host, region):
     data = json.loads(out.decode('utf-8'))
 
     opts = []
+    endpoints = []
     for row in data:
         if row['state'] == 'RUNNING':
             ip = row['private_ip']
@@ -25,9 +26,12 @@ def cli(stack_name, port, jump_host, region):
                 hostname = 'ip-{}.{}.compute.internal'.format(ip.replace('.', '-'), region)
                 subprocess.call(['sudo', 'su', '-c', 'echo "{} {}" >> /etc/hosts'.format(ip, hostname)])
                 opts += ['-L', '{}:{}:{}:{}'.format(ip, port, ip, port)]
+                endpoints.append('{}:{}'.format(ip, port))
 
-    if not opts:
+    if not endpoints:
         raise click.UsageError('No instances for Senza stack "{}" found.'.format(stack_name))
+
+    click.secho('Endpoints: {}'.format(','.join(endpoints)), bold=True, fg='blue')
 
     click.secho('Starting SSH tunnels..', bold=True)
     subprocess.call(['ssh'] + opts + [jump_host, 'while true; do echo -n .; sleep 60; done'])
